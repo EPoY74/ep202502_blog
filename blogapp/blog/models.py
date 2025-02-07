0fad1a57b0746b177a1e25ca63127aef658f23ce
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-02-07 16:22:32 +0500

    Добавил поля со временем и мета класс

--- blogapp/blog/models.py
+++ blogapp/blog/models.py
@@ -1,11 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Post(models.Model):
-    title=models.CharField(max_length=100)
-    slug=models.SlugField(max_length=100)
+    title=models.CharField(max_length=100, unique=True)
+    slug=models.SlugField(max_length=100, unique=True)
     body=models.TextField
+    publish=models.DateTimeField(default=timezone.now)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+
+
+class Meta:
+    ordering=['-publish']
+    indexes=[
+        models.Index(fields=['-publish'])
+    ]
+    verbose_name="Пост"
+    verbose_name_plural="Посты"
     
 def __str__(self):
     return self.title
