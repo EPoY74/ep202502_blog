b78a0dab3e9621e6bb297ac6699b3bd55f786192
EugeniiPetrov_eM350 <epoy74@gmail.com>
2025-02-09 09:00:20 +0500

    добавил обновленный вывод модели Post в админке комминтарием

--- blogapp/blog/admin.py
+++ blogapp/blog/admin.py
@@ -3,4 +3,18 @@ from .models import Post
 
 # Register your models here.
 
-admin.site.register(Post)
+# admin.site.register(Post) перваяверсия.Сенйчасбудет расширенная
+
+# выполняет ту же функцию, что и admin.site.register()  
+@admin.register(Post)
+
+# НАследуюсьоткласса, чтобы добавлять инфо, как 
+# отображать модель и как с ней взаимодействовать
+class PostAdmin(admin.ModelAdmin):
+    
+    # показываемполя модели которые мы хотим видеть в админке
+    list_display=['title', 
+    'slug',
+    'author', 
+    'publish',
+    'status',]
