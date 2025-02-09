d6a2af7fe0774aafcd54062941da58d50072a63a
EugeniiPetrov_eM350 <epoy74@gmail.com>
2025-02-09 10:33:57 +0500

    Создалкомментарий прозапуск приложения моего

--- blogapp/blogapp/settings.py
+++ blogapp/blogapp/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    # указываю.чтоуменя есть приложение, чтобы django 
+    # запустил его
     'blog.BlogConfig',
 ]
 
