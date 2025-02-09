095c73fb4e1257989d644422b268af1709245b5c
EugeniiPetrov_eM350 <epoy74@gmail.com>
2025-02-09 08:46:01 +0500

    добавил verbose_name в модель

--- blogapp/blog/models.py
+++ blogapp/blog/models.py
@@ -4,36 +4,38 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-    
+
 class Post(models.Model):
-    
-    
     class Status(models.TextChoices):
         DRAFT='DF','Черновик'
         PUBLISHED='PB','Опублковано'
     
     
-    title=models.CharField(max_length=100, unique=True)
-    slug=models.SlugField(max_length=100, unique=True)
+    title=models.CharField(max_length=100, unique=True, verbose_name='Заголовок')
+    slug=models.SlugField(max_length=100, unique=True, verbose_name='Слаг(путь)')
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
-                               related_name='blog_posts')
-    
-    body=models.TextField()
-    publish=models.DateTimeField(default=timezone.now)
-    created=models.DateTimeField(auto_now_add=True)
-    updated=models.DateTimeField(auto_now=True)
+                               related_name='blog_posts',
+                               verbose_name='Автор')
+
+    body=models.TextField(verbose_name='Текст поста')
+    publish=models.DateTimeField(default=timezone.now, verbose_name='Дата публикации')
+    created=models.DateTimeField(auto_now_add=True,verbose_name='Дата создания')
+    updated=models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
     status = models.CharField(max_length=2,
                               choices=Status.choices,
-                              default=Status.DRAFT)
+                              default=Status.DRAFT,
+                              verbose_name='Статус поста')
+
     class Meta:
+        """"""
         ordering=['-publish']
         indexes=[
             models.Index(fields=['publish']),
         ]
         verbose_name="Пост"
         verbose_name_plural="Посты"
-    
+
     def __str__(self):
-        return self.title
-    
+        return str(self.title)
+
