bb454128dfe6336112d9e93e900f0784fc2284c9
EugeniiPetrov_eM350 <epoy74@gmail.com>
2025-02-10 16:08:16 +0500

    Добавил модель для вывода только опубликованных постов

--- blogapp/blog/models.py
+++ blogapp/blog/models.py
@@ -3,6 +3,10 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+            .filter(status=Post.Status.PUBLISHED)
 
 
 class Post(models.Model):
