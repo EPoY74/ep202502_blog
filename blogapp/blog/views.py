18543288d2d220c23b5a064c8a9f02ede656a91f
EugeniiPetrov_eM350 <epoy74@gmail.com>
2025-02-11 08:45:48 +0500

    init реализация представления

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
+
+def post_list(request):
+    post = Post.published.all()
+    return render(request,
+                  {'blog/post/list.html'},
+                  {'posts':post})
 # Create your views here.
