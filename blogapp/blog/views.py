bbc5b37dc0fe0f1a917aa777140e5e427405166b
EugeniiPetrov_eM350 <epoy74@gmail.com>
2025-02-12 08:51:02 +0500

    поправил ошибки метода, как будет работать - не знаю.В Книге был не корректный код

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from django.core.exceptions import ObjectDoesNotExist
 
 from django.shortcuts import render
 from .models import Post
@@ -11,11 +12,12 @@ def post_list(request):
                   {'posts':post})
 # Create your views here.
 
-def post_detail(request, id):
+def post_detail(request, post_id):
     try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No post found")
+        post = Post.published.get(id=post_id)
+    except ObjectDoesNotExist as exc:
+        print(f"{exc}")
+        raise Http404("No post found") from exc
 
     return render(request,
                   'blog/post/detail.html', 
