cd9ca77bbbb57de88aad970e0dd48880722e8875
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-02-16 00:10:48 +0500

    init

---
+++ blogapp/blog/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class EmailPostForm(forms.Form):
+    """
+    Класс для отравки уведомлений по электронной почте
+    """
+    name = forms.CharField(max_length=25)
+    email = forms.EmailField()
+    to = forms.EmailField()
+    comments = forms.CharField(required=False,
+                               widget=forms.Textarea) 
