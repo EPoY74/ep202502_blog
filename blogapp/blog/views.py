12f381c14b4a38dc2112ada09188dbf55b68e0fa
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-02-15 22:55:45 +0500

    реализация пагинации. и лишние файлы чуть

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import get_object_or_404
 
 from django.shortcuts import render
 from .models import Post
+from django.core.paginator import Paginator
 
 
 def post_list(request):
-    post = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+    
     return render(request,
                   'blog/post/list.html',
-                  {'posts':post})
+                  {'posts':posts})
 # Create your views here.
 
 
